e1ee5656b9e2f4d7c3d8ffc81ff9fe349a4e4d1a
Oleksii Babych <babych.oleksii@gmail.com>
2022-08-20 18:51:15 +0300

    Type hinting

--- 02_Python_Refresher/41_type_hinting.py
+++ 02_Python_Refresher/41_type_hinting.py
@@ -15,7 +15,7 @@ class Book:
 
 
 class BookShelf:
-    def __init__(self, books:List[Book]):
+    def __init__(self, books: List[Book]):
         self.books = books
 
     def __str__(self) -> str:
---
+++ 02_Python_Refresher/42_imports/mymodule.py
@@ -0,0 +1,6 @@
+# from imports_code import divide
+
+import imports_code
+
+print(divide(10, 2))
+
